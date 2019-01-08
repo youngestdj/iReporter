c9a1e399fdd7745b426a6ab7b2e69111ee6958b3
youngestdj <jessam7007@gmail.com>
2019-01-08 22:14:02 +0100

    feat(api): consume signup endpoint
    - make api call to signup endpoint
    - save user token and user data in cookies
    
    [Delivers #163072056]

---
+++ UI/js/cookies.js
@@ -0,0 +1,36 @@
+const setCookie = (name, value, days) => {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value}; ${expires}; path=/`;
+};
+
+const getCookie = (cookieName) => {
+  const name = cookieName + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArr = decodedCookie.split(';');
+  for(let i = 0; i < cookieArr.length; i++) {
+    let j = cookieArr[i];
+    while (j.charAt(0) === ' ') {
+      j = cookieArr.substring(1);
+    }
+    if (j.indexOf(name) === 0) return j.substring(name.length, j.length);
+  }
+  return "";
+};
+
+const checkCookie = (cookieName) => {
+  if(getCookie(cookieName) !== '') {
+    return true;
+  } else return false;
+};
+
+const extendCookie = (name, value, days) => {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value}; ${expires}; path=/`;
+};
+
+//(!checkCookie('iReporterToken'))? window.location.replace("login.html") : extendCookie('iReporterToken', getCookie('iReporterToken'), 30);
+;
